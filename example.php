31b047a5acc50fd7818d9df1cbcb7f50b9fb528d
Kevin Khandjian <kevinkhandjian@gmail.com>
2012-09-25 14:56:07 -0400

    Updated example.php

--- example.php
+++ example.php
@@ -4,20 +4,17 @@
 	
 	$client_id = '';
 	$client_secret = '';
-	$callback_url = 'http://127.0.0.1/callback'; //must be set in buffer's app registration settings
+	$callback_url = 'http://127.0.0.1/callback';
 	
-	//as long as this next line is executed on the $callback_url request you'll be logged in fine
 	$buffer = new BufferApp($client_id, $client_secret, $callback_url);
 		
 	if (!$buffer->ok) {
 		echo '<a href="' . $buffer->get_login_url() . '">Connect to Buffer!</a>';
 	} else {
-		//this pulls all of the logged in user's profiles
 		$profiles = $buffer->go('/profiles');
 			
 		if (is_array($profiles)) {
 			foreach ($profiles as $profile) {
-				//this creates a status on each one
 				$buffer->go('/updates/create', array('text' => 'My first status update from bufferapp-php worked!', 'profile_ids[]' => $profile->id));
 			}
 		}
