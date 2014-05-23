3706f53e689867aa25a77ebb0813b6a5a724e1bc
Patrick Reilly <preilly@php.net>
2014-05-23 10:22:50 -0700

    update example

--- example.php
+++ example.php
@@ -1,22 +1,27 @@
-<?
-	session_start();
-	require('buffer.php');
-	
-	$client_id = '';
-	$client_secret = '';
-	$callback_url = 'http://127.0.0.1/callback';
-	
-	$buffer = new BufferApp($client_id, $client_secret, $callback_url);
-		
-	if (!$buffer->ok) {
-		echo '<a href="' . $buffer->get_login_url() . '">Connect to Buffer!</a>';
-	} else {
-		$profiles = $buffer->go('/profiles');
-			
-		if (is_array($profiles)) {
-			foreach ($profiles as $profile) {
-				$buffer->go('/updates/create', array('text' => 'My first status update from bufferapp-php worked!', 'profile_ids[]' => $profile->id));
-			}
-		}
-	}
-?>
+<?php
+
+  require 'vendor/autoload.php';
+
+  use Buffer\App\BufferApp;
+
+  session_start();
+
+  $client_id = '';
+  $client_secret = '';
+  $callback_url = 'http://127.0.0.1/callback';
+
+  $buffer = new BufferApp($client_id, $client_secret, $callback_url);
+
+  if (!$buffer->ok) {
+    echo '<a href="' . $buffer->get_login_url() . '">Connect to Buffer!</a>';
+  } else {
+    $profiles = $buffer->go('/profiles');
+
+    if (is_array($profiles)) {
+      foreach ($profiles as $profile) {
+        $buffer->go('/updates/create', array(
+          'text' => 'My first status update from BufferApp worked!',
+          'profile_ids[]' => $profile->id));
+      }
+    }
+  }
